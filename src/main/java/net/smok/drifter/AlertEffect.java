0b29ec458d4c749a7d9bedcc78ed4160e0e427ac
Smok419 <danilmaksimovih@gmail.com>
2025-08-26 20:31:48 +0300

    Add Alert effect

---
+++ src/main/java/net/smok/drifter/AlertEffect.java
@@ -0,0 +1,11 @@
+package net.smok.drifter;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class AlertEffect extends MobEffect {
+    public AlertEffect(MobEffectCategory mobEffectCategory, int i) {
+        super(mobEffectCategory, i);
+    }
+
+}
