a7a25de8480e1b8a3c5c53a1068529433ce86788
Smok419 <danilmaksimovih@gmail.com>
2025-09-28 12:37:43 +0300

    Add shape, model and texture to AlertPanelBlock

--- src/main/java/net/smok/drifter/blocks/alert/AlertPanelBlock.java
+++ src/main/java/net/smok/drifter/blocks/alert/AlertPanelBlock.java
@@ -3,25 +3,43 @@ package net.smok.drifter.blocks.alert;
 import earth.terrarium.botarium.common.menu.ExtraDataMenuProvider;
 import earth.terrarium.botarium.common.menu.MenuHooks;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.smok.drifter.registries.DrifterBlocks;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class AlertPanelBlock extends BaseEntityBlock {
+
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    public static final VoxelShape SHAPE_SOUTH = box(1, 1, 0, 15, 15, 6);
+    public static final VoxelShape SHAPE_NORTH = box(1, 1, 10, 15, 15, 16);
+    public static final VoxelShape SHAPE_EAST = box(0, 1, 1, 6, 15, 15);
+    public static final VoxelShape SHAPE_WEST = box(10, 1, 1, 16, 15, 15);
+
+
     public AlertPanelBlock(Properties properties) {
         super(properties);
+        registerDefaultState(getStateDefinition().any().setValue(FACING, Direction.NORTH));
     }
 
     @Nullable
@@ -35,6 +53,32 @@ public class AlertPanelBlock extends BaseEntityBlock {
         return RenderShape.MODEL;
     }
 
+    @Override
+    public @NotNull VoxelShape getShape(@NotNull BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return switch (state.getValue(FACING)) {
+            case NORTH -> SHAPE_NORTH;
+            case SOUTH -> SHAPE_SOUTH;
+            case EAST -> SHAPE_EAST;
+            case WEST -> SHAPE_WEST;
+            default -> SHAPE_NORTH;
+        };
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        for (Direction dir : context.getNearestLookingDirections()) {
+            if (dir.getAxis() == Direction.Axis.Y) continue;
+
+            BlockState blockState = defaultBlockState().setValue(FACING, dir.getOpposite());
+            if (canSurvive(blockState, context.getLevel(), context.getClickedPos())) return blockState;
+        }
+        return null;
+    }
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
 
     @Override
     public InteractionResult use(BlockState blockState, @NotNull Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult) {
@@ -44,11 +88,33 @@ public class AlertPanelBlock extends BaseEntityBlock {
         return InteractionResult.CONSUME;
     }
 
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return FaceAttachedHorizontalDirectionalBlock.canAttach(level, pos, state.getValue(FACING).getOpposite());
+    }
+
+    @Override
+    public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+        return state.getValue(FACING).getOpposite() == direction && !state.canSurvive(level, pos) ?
+                Blocks.AIR.defaultBlockState() :
+                super.updateShape(state, direction, neighborState, level, pos, neighborPos);
+    }
+
     @Nullable
     @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType) {
         return blockEntityType == DrifterBlocks.ALERT_PANEL_BLOCK_ENTITY.get() ? (level1, blockPos, blockState1, blockEntity) -> {
             if (!level1.isClientSide) ((AlertPanelBlockEntity)blockEntity).tick(level1);
         } : null;
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return state.rotate(mirror.getRotation(state.getValue(FACING)));
     }
 }
