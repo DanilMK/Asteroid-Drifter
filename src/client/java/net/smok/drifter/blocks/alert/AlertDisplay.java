e26c0551a591c3bd53a0a6303d7b766da3dc552f
Smok419 <danilmaksimovih@gmail.com>
2026-01-06 13:22:56 +0300

    Move main methods from AlertDisplayWidget to outer class AlertDisplay.

---
+++ src/client/java/net/smok/drifter/blocks/alert/AlertDisplay.java
@@ -0,0 +1,113 @@
+package net.smok.drifter.blocks.alert;
+
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.sounds.SoundEvents;
+import net.smok.drifter.network.NetworkHandler;
+import net.smok.drifter.registries.Values;
+import net.smok.drifter.widgets.Hovered;
+import net.smok.drifter.widgets.Sprite;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+
+public record AlertDisplay(Alert alert, Font font) {
+
+    public static final ResourceLocation TEXTURE = new ResourceLocation(Values.MOD_ID, "textures/gui/alert/alert_buttons.png");
+
+    public static final Sprite TEXT = Sprite.of(TEXTURE, 0, 0, 100, 20, 100, 100);
+    public static final Sprite PLUS = Sprite.of(TEXTURE, 0, 20, 20, 20, 100, 100);
+    public static final Sprite MINUS = Sprite.of(TEXTURE, 20, 20, 20, 20, 100, 100);
+    public static final Sprite TEST_OFF = Sprite.of(TEXTURE, 40, 20, 20, 20, 100, 100);
+    public static final Sprite TEST_OFF_PRESS = Sprite.of(TEXTURE, 40, 40, 20, 20, 100, 100);
+    public static final Sprite TEST_ON = Sprite.of(TEXTURE, 60, 20, 20, 20, 100, 100);
+    public static final Sprite TEST_ON_PRESS = Sprite.of(TEXTURE, 60, 40, 20, 20, 100, 100);
+    public static final Sprite ACTIVE = Sprite.of(TEXTURE, 80, 40, 20, 20, 100, 100);
+    public static final Sprite ACTIVE_OFF = Sprite.of(TEXTURE, 80, 20, 20, 20, 100, 100);
+    public static final Sprite BUTTON = Sprite.of(TEXTURE, 0, 60, 60, 20, 100, 100);
+    public static final Sprite BLANK = Sprite.of(TEXTURE, 60, 60, 20, 20, 100, 100);
+    public static final Sprite BLANK_PRESS = Sprite.of(TEXTURE, 60, 80, 20, 20, 100, 100);
+    public static final Sprite EMPTY = Sprite.of(TEXTURE, 80, 80, 20, 20, 100, 100);
+    public static final Sprite UP = Sprite.of(TEXTURE, 80, 60, 10, 10, 100, 100);
+    public static final Sprite UP_PRESS = Sprite.of(TEXTURE, 80, 70, 10, 10, 100, 100);
+    public static final Sprite DOWN = Sprite.of(TEXTURE, 90, 60, 10, 10, 100, 100);
+    public static final Sprite DOWN_PRESS = Sprite.of(TEXTURE, 90, 70, 10, 10, 100, 100);
+
+    public static final Sprite BACKGROUND = Sprite.ofName("alert/alert_system_gui.png", 196, 201);
+
+    public void test(BlockPos blockPos, int index) {
+        alert().setTested(!alert().isTested());
+        Minecraft.getInstance().getSoundManager()
+                .play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0F));
+        ClientPlayNetworking.send(NetworkHandler.DETECTOR_TEST.getId(), NetworkHandler.DETECTOR_TEST.createPacket(blockPos, index, alert().isTested()).getData());
+    }
+
+    public void swap(BlockPos blockPos, int indexA, int indexB) {
+        ClientPlayNetworking.send(NetworkHandler.DETECTOR_MOVE.getId(), NetworkHandler.DETECTOR_MOVE.createPacket(blockPos, indexA, indexB).getData());
+    }
+
+    public void editSound(@NotNull Minecraft minecraft, Screen parent, BlockPos blockPos, int index) {
+        minecraft.setScreen(new AlertSoundEditScreen(parent, alert.getSound(), s ->
+                ClientPlayNetworking.send(NetworkHandler.DETECTOR_SOUND.getId(),
+                        NetworkHandler.DETECTOR_SOUND.createPacket(blockPos, index, s).getData())));
+    }
+
+    public void editName(@NotNull Minecraft minecraft, Screen parent, BlockPos blockPos, int index) {
+        minecraft.setScreen(new AlertNameEditScreen(parent, alert.getName(), s ->
+                ClientPlayNetworking.send(NetworkHandler.DETECTOR_NAME.getId(),
+                        NetworkHandler.DETECTOR_NAME.createPacket(blockPos, index, s).getData())));
+    }
+
+    public void editIcon(@NotNull Minecraft minecraft, Screen parent, BlockPos blockPos, int index) {
+        minecraft.setScreen(new AlertIconEditScreen(parent, alert.getIcon(), i ->
+                ClientPlayNetworking.send(NetworkHandler.DETECTOR_ICON.getId(),
+                        NetworkHandler.DETECTOR_ICON.createPacket(blockPos, index, i).getData())));
+    }
+
+    public static void renderIcon(@NotNull GuiGraphics guiGraphics, @NotNull Icon icon, int x, int y) {
+        if (icon.isPaintIcon()) {
+            Color color = icon.getColor();
+            guiGraphics.setColor(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, 1f);
+        }
+        if (icon.getAsItem() != null) {
+            guiGraphics.renderItem(icon.getAsItem(), x + 1, y + 1);
+        } else if (icon.getAsMobEffect() != null) {
+            TextureAtlasSprite sprite = Minecraft.getInstance().getMobEffectTextures().get(icon.getAsMobEffect());
+            guiGraphics.blit(x, y, 0, 18, 18, sprite);
+        }
+        if (icon.isPaintIcon()) guiGraphics.setColor(1, 1, 1, 1);
+    }
+
+    public void renderAt(@NotNull GuiGraphics guiGraphics, int x, int y, boolean focusing, int mouseX, int mouseY) {
+        BLANK.draw(guiGraphics, x, y);
+        renderIcon(guiGraphics, alert().getIcon(), x + 1, y + 1);
+        x += BLANK.width();
+        TEXT.draw(guiGraphics, x, y);
+        guiGraphics.drawString(font(), alert().text(), x + 4, y + 6, Integer.MAX_VALUE);
+
+        x += TEXT.width();
+        if (alert().isTested()) {
+            if (focusing || Hovered.isHover(x, y, x + TEST_ON.width(), y + TEST_ON.height(), mouseX, mouseY))
+                TEST_ON_PRESS.draw(guiGraphics, x, y);
+            else TEST_ON.draw(guiGraphics, x, y);
+        } else {
+            if (focusing || Hovered.isHover(x, y, x + TEST_ON.width(), y + TEST_ON.height(), mouseX, mouseY))
+                TEST_OFF_PRESS.draw(guiGraphics, x, y);
+            else TEST_OFF.draw(guiGraphics, x, y);
+        }
+
+        x += TEST_ON.width();
+        if (alert().isActive()) {
+            ACTIVE.draw(guiGraphics, x, y);
+        } else {
+            ACTIVE_OFF.draw(guiGraphics, x, y);
+        }
+    }
+}
--- src/client/java/net/smok/drifter/blocks/alert/AlertDisplayWidget.java
+++ src/client/java/net/smok/drifter/blocks/alert/AlertDisplayWidget.java
@@ -7,65 +7,40 @@ import net.minecraft.client.gui.components.AbstractWidget;
 import net.minecraft.client.gui.narration.NarratedElementType;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.gui.navigation.CommonInputs;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
-import net.smok.drifter.registries.Values;
 import net.smok.drifter.widgets.Hovered;
-import net.smok.drifter.widgets.Sprite;
 import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
-import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class AlertDisplayWidget extends AbstractWidget {
 
 
-    private static final ResourceLocation TEXTURE = new ResourceLocation(Values.MOD_ID, "textures/gui/alert/alert_buttons.png");
-
-    public static final Sprite TEXT = Sprite.of(TEXTURE, 0, 0, 100, 20, 100, 100);
-    public static final Sprite PLUS = Sprite.of(TEXTURE, 0, 20, 20, 20, 100, 100);
-    public static final Sprite MINUS = Sprite.of(TEXTURE, 20, 20, 20, 20, 100, 100);
-    public static final Sprite TEST_OFF = Sprite.of(TEXTURE, 40, 20, 20, 20, 100, 100);
-    public static final Sprite TEST_OFF_PRESS = Sprite.of(TEXTURE, 40, 40, 20, 20, 100, 100);
-    public static final Sprite TEST_ON = Sprite.of(TEXTURE, 60, 20, 20, 20, 100, 100);
-    public static final Sprite TEST_ON_PRESS = Sprite.of(TEXTURE, 60, 40, 20, 20, 100, 100);
-    public static final Sprite ACTIVE = Sprite.of(TEXTURE, 80, 40, 20, 20, 100, 100);
-    public static final Sprite ACTIVE_OFF = Sprite.of(TEXTURE, 80, 20, 20, 20, 100, 100);
-    public static final Sprite BUTTON = Sprite.of(TEXTURE, 0, 60, 60, 20, 100, 100);
-    public static final Sprite BLANK = Sprite.of(TEXTURE, 60, 60, 20, 20, 100, 100);
-    public static final Sprite BLANK_PRESS = Sprite.of(TEXTURE, 60, 80, 20, 20, 100, 100);
-    public static final Sprite EMPTY = Sprite.of(TEXTURE, 80, 80, 20, 20, 100, 100);
-    public static final Sprite UP = Sprite.of(TEXTURE, 80, 60, 10, 10, 100, 100);
-    public static final Sprite UP_PRESS = Sprite.of(TEXTURE, 80, 70, 10, 10, 100, 100);
-    public static final Sprite DOWN = Sprite.of(TEXTURE, 90, 0, 60, 10, 100, 100);
-    public static final Sprite DOWN_PRESS = Sprite.of(TEXTURE, 90, 70, 10, 10, 100, 100);
-
-    private final Alert alert;
-    private final Consumer<Boolean> onTestClick;
     private final Supplier<Boolean> shouldDrawUp, shouldDrawDown;
     private final Runnable up, down;
-    private final Font font;
     private final boolean canBeSelected;
+    private final AlertDisplay alertDisplay;
+    private final BlockPos detectorPos;
+    private final int index;
 
-    public AlertDisplayWidget(int x, int y, Alert alert, Font font, Consumer<Boolean> onTestClick, boolean canBeSelected, Supplier<Boolean> shouldDrawUp, Supplier<Boolean> shouldDrawDown, Runnable up, Runnable down) {
+    public AlertDisplayWidget(int x, int y, Alert alert, Font font, boolean canBeSelected, Supplier<Boolean> shouldDrawUp, Supplier<Boolean> shouldDrawDown, Runnable up, Runnable down, BlockPos detectorPos, int index) {
         super(x, y, 160, 20, Component.empty());
-        this.alert = alert;
-        this.onTestClick = onTestClick;
+        this.detectorPos = detectorPos;
+        this.index = index;
+        alertDisplay = new AlertDisplay(alert, font);
         this.shouldDrawUp = shouldDrawUp;
         this.shouldDrawDown = shouldDrawDown;
         this.up = up;
         this.down = down;
-        this.font = font;
         this.canBeSelected = canBeSelected;
     }
 
-    public AlertDisplayWidget(int x, int y, Alert alert, Font font, Consumer<Boolean> onTestClick) {
-        this(x, y, alert, font, onTestClick, false, () -> false, () -> false, () -> {}, () -> {});
+    public AlertDisplayWidget(int x, int y, Alert alert, Font font, BlockPos detectorPos, int index) {
+        this(x, y, alert, font, false, () -> false, () -> false, () -> {}, () -> {}, detectorPos, index);
     }
 
     @Override @Deprecated
@@ -74,73 +49,54 @@ public class AlertDisplayWidget extends AbstractWidget {
     @Override
     protected void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
         int x = getX();
-        BLANK.draw(guiGraphics, x, getY());
-        renderIcon(guiGraphics, alert.getIcon(), x + 1, getY() + 1);
-        x += BLANK.width();
-        TEXT.draw(guiGraphics, x, getY());
-        renderScrollingString(guiGraphics, font, getMessage(), x, getY(), x + TEXT.width(), getY() + getHeight(), Integer.MAX_VALUE);
-
-        x += TEXT.width();
-        if (alert.isTested()) {
-            if (isTestHovered(mouseX, mouseY) || (isFocused() && !canBeSelected)) TEST_ON_PRESS.draw(guiGraphics, x, getY());
-            else TEST_ON.draw(guiGraphics, x, getY());
-        } else {
-            if (isTestHovered(mouseX, mouseY) || (isFocused() && !canBeSelected)) TEST_OFF_PRESS.draw(guiGraphics, x, getY());
-            else TEST_OFF.draw(guiGraphics, x, getY());
-        }
-
-        x += TEST_ON.width();
-        if (alert.isActive()) {
-            ACTIVE.draw(guiGraphics, x, getY());
-        } else {
-            ACTIVE_OFF.draw(guiGraphics, x, getY());
-        }
+        int y = getY();
+        alertDisplay.renderAt(guiGraphics, x, y, isFocused(), mouseX, mouseY);
 
         if (shouldDrawDown.get()) {
-            if (isDownHovered(mouseX, mouseY)) DOWN_PRESS.draw(guiGraphics, getX(), getY() + 10);
-            else DOWN.draw(guiGraphics, getX(), getY() + 10);
+            if (isDownHovered(getX(), getY(), mouseX, mouseY)) AlertDisplay.DOWN_PRESS.draw(guiGraphics, getX(), y + 10);
+            else AlertDisplay.DOWN.draw(guiGraphics, getX(), y + 10);
         }
 
         if (shouldDrawUp.get()) {
-            if (isUpHovered(mouseX, mouseY)) UP_PRESS.draw(guiGraphics, getX(), getY());
-            else UP.draw(guiGraphics, getX(), getY());
+            if (isUpHovered(getX(), getY(), mouseX, mouseY)) AlertDisplay.UP_PRESS.draw(guiGraphics, getX(), y);
+            else AlertDisplay.UP.draw(guiGraphics, getX(), y);
         }
 
         if (canBeSelected && isHoveredOrFocused()) {
-            guiGraphics.hLine(getX(), getX() + getWidth(), getY(), Integer.MAX_VALUE);
-            guiGraphics.hLine(getX(), getX() + getWidth(), getY() + getHeight() - 1, Integer.MAX_VALUE);
-            guiGraphics.vLine(getX(), getY(), getY() + getHeight(), Integer.MAX_VALUE);
-            guiGraphics.vLine(getX() + getWidth() - 1, getY(), getY() + getHeight(), Integer.MAX_VALUE);
+            guiGraphics.hLine(getX(), getX() + getWidth(), y, Integer.MAX_VALUE);
+            guiGraphics.hLine(getX(), getX() + getWidth(), y + getHeight() - 1, Integer.MAX_VALUE);
+            guiGraphics.vLine(getX(), y, y + getHeight(), Integer.MAX_VALUE);
+            guiGraphics.vLine(getX() + getWidth() - 1, y, y + getHeight(), Integer.MAX_VALUE);
         }
 
     }
 
-    private boolean isTestHovered(int mouseX, int mouseY) {
-        return Hovered.isHover(getX() + 120, getY(), getX() + 140, getY() + 20, mouseX, mouseY);
+    private boolean isTestHovered(int x, int y, int mouseX, int mouseY) {
+        return Hovered.isHover(x + 120, y, x + 140, y + 20, mouseX, mouseY);
     }
 
-    private boolean isUpHovered(int mouseX, int mouseY) {
-        return Hovered.isHover(getX(), getY(), getX() + 20, getY() + 10, mouseX, mouseY);
+    private boolean isUpHovered(int x, int y, int mouseX, int mouseY) {
+        return Hovered.isHover(x, y, x + 20, y + 10, mouseX, mouseY);
     }
 
-    private boolean isDownHovered(int mouseX, int mouseY) {
-        return Hovered.isHover(getX(), getY() + 10, getX() + 20, getY() + 20, mouseX, mouseY);
+    private boolean isDownHovered(int x, int y, int mouseX, int mouseY) {
+        return Hovered.isHover(x, y + 10, x + 20, y + 20, mouseX, mouseY);
     }
 
     @Override
     public @NotNull Component getMessage() {
-        return alert.text();
+        return alertDisplay.alert().text();
     }
 
     @Override
     protected boolean clicked(double mouseX, double mouseY) {
-        return canBeSelected && super.clicked(mouseX, mouseY) || isTestHovered((int) mouseX, (int) mouseY);
+        return canBeSelected && super.clicked(mouseX, mouseY) || isTestHovered(getX(), getY(), (int) mouseX, (int) mouseY);
     }
 
     @Override
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
         if (this.active && this.visible && CommonInputs.selected(keyCode)) {
-            test();
+            alertDisplay.test(detectorPos, index);
             return true;
         }
         return false;
@@ -150,17 +106,10 @@ public class AlertDisplayWidget extends AbstractWidget {
     public void onClick(double mouseX, double mouseY) {
         int mx = (int) mouseX;
         int my = (int) mouseY;
-        if (isTestHovered(mx, my)) test();
-        else if (shouldDrawUp.get() && isUpHovered(mx, my)) up.run();
-        else if (shouldDrawDown.get() && isDownHovered(mx, my)) down.run();
+        if (isTestHovered(getX(), getY(), mx, my)) alertDisplay.test(detectorPos, index);
+        else if (shouldDrawUp.get() && isUpHovered(getX(), getY(), mx, my)) up.run();
+        else if (shouldDrawDown.get() && isDownHovered(getX(), getY(), mx, my)) down.run();
         else select();
-    }
-
-    private void test() {
-        alert.setTested(!alert.isTested());
-        onTestClick.accept(alert.isTested());
-        Minecraft.getInstance().getSoundManager()
-                .play(SimpleSoundInstance.forUI(SoundEvents.UI_BUTTON_CLICK, 1.0F));
     }
 
     private void select() {
@@ -177,17 +126,4 @@ public class AlertDisplayWidget extends AbstractWidget {
         narrationElementOutput.add(NarratedElementType.HINT, getMessage());
     }
 
-    public static void renderIcon(@NotNull GuiGraphics guiGraphics, @NotNull Icon icon, int x, int y) {
-        if (icon.isPaintIcon()) {
-            Color color = icon.getColor();
-            guiGraphics.setColor(color.getRed() / 255f, color.getGreen() / 255f, color.getBlue() / 255f, 1f);
-        }
-        if (icon.getAsItem() != null) {
-            guiGraphics.renderItem(icon.getAsItem(), x + 1, y + 1);
-        } else if (icon.getAsMobEffect() != null) {
-            TextureAtlasSprite sprite = Minecraft.getInstance().getMobEffectTextures().get(icon.getAsMobEffect());
-            guiGraphics.blit(x, y, 0, 18, 18, sprite);
-        }
-        if (icon.isPaintIcon()) guiGraphics.setColor(1, 1, 1, 1);
-    }
 }
